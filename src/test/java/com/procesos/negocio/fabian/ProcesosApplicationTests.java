cb5b18489f002a540cb39c78332c7c9760d1e896
Fabian <fabianrincon1407@gmail.com>
2022-09-15 19:52:34 -0500

    Commit inicial spring

---
+++ src/test/java/com/procesos/negocio/fabian/ProcesosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procesos.negocio.fabian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
