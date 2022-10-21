4cde8cadc7966cb77cb7ddf34be48d38626901ad
Fabian <fabianrincon1407@gmail.com>
2022-10-20 21:35:25 -0500

    separacion logica del negocio a la capa service y validacion de la peticion con @valid

--- src/main/java/com/procesos/negocio/fabian/controllers/UsuarioController.java
+++ src/main/java/com/procesos/negocio/fabian/controllers/UsuarioController.java
@@ -2,28 +2,27 @@ package com.procesos.negocio.fabian.controllers;
 
 import com.procesos.negocio.fabian.models.Usuario;
 import com.procesos.negocio.fabian.repository.UsuarioRepository;
+import com.procesos.negocio.fabian.services.UsuarioService;
+import com.procesos.negocio.fabian.services.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import javax.validation.Valid;
+import java.util.*;
 
 @RestController
 public class UsuarioController {
 
     @Autowired
-    private UsuarioRepository usuarioRepository;
+   private UsuarioService usuarioService;
     @GetMapping(value = "/usuario/{id}")
     public ResponseEntity getUsuario(@PathVariable Long id){
-      Optional<Usuario> usuario = usuarioRepository.findById(id);
-      if(usuario.isPresent())
-      {
-          return new ResponseEntity(usuario, HttpStatus.OK);
-      }
-      return  ResponseEntity.notFound().build();
+        return usuarioService.getUserById(id);
 
        /* Usuario usuario= new Usuario();
         usuario.setId(id);
@@ -36,76 +35,44 @@ public class UsuarioController {
 
     }
     @PostMapping("/usuario")
-    public ResponseEntity crearUsuario(@RequestBody Usuario usuario){
-        try {
-            usuarioRepository.save(usuario);
-            return new ResponseEntity(usuario, HttpStatus.CREATED);
-        }catch (Exception e) {
-            return ResponseEntity.badRequest().build();
-        }
+    public ResponseEntity crearUsuario(@Valid @RequestBody  Usuario usuario){
+        return usuarioService.createUser(usuario);
     }
     @GetMapping(value="/usuarios")
     public ResponseEntity listarUsuarios(){
-    List<Usuario> usuarios =usuarioRepository.findAll();
-    if(usuarios.isEmpty()){
-        return ResponseEntity.notFound().build();
-    }
-    return  new ResponseEntity(usuarios, HttpStatus.OK);
+        return usuarioService.allUsers();
     }
 
     @GetMapping(value="/usuarios/{nombre}/{apellidos}")
     public ResponseEntity listarPorNombreApellidos(@PathVariable  String nombre, @PathVariable String apellidos ){
-        List<Usuario> usuarios= usuarioRepository.findAllByNombreAndApellidos(nombre,apellidos);;
-        if(usuarios.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }
-        return  new ResponseEntity(usuarios, HttpStatus.OK);
+        return usuarioService.allUsersByNameAndLastName(nombre,apellidos);
     }
     @GetMapping(value="/usuarios/nombre/{nombre}")
     public ResponseEntity listarPorNombre(@PathVariable  String nombre ){
-        List<Usuario> usuarios= usuarioRepository.findAllByNombre(nombre);
-        if(usuarios.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }
-        return  new ResponseEntity(usuarios, HttpStatus.OK);
+        return usuarioService.allUsersByName(nombre);
     }
     @GetMapping(value="/usuarios/apellidos/{apellidos}")
     public ResponseEntity listarPorapellidos(@PathVariable  String apellidos ){
-        List<Usuario> usuarios= usuarioRepository.findAllByApellidos(apellidos);
-        if(usuarios.isEmpty()){
-            return ResponseEntity.notFound().build();
-        }
-        return  new ResponseEntity(usuarios, HttpStatus.OK);
+        return usuarioService.allUsersByLastName(apellidos);
     }
     @PutMapping(value = "/usuario/{id}")
-    public ResponseEntity editarUsuario(@PathVariable Long id,@RequestBody Usuario usuario){
-            Optional<Usuario> usuarioBD =usuarioRepository.findById(id);
-        if(usuarioBD.isPresent()){
-            try{
-                usuarioBD.get().setNombre(usuario.getNombre());
-                usuarioBD.get().setApellidos(usuario.getApellidos());
-                usuarioBD.get().setDireccion(usuario.getDireccion());
-                usuarioBD.get().setDocumento(usuario.getDocumento());
-                usuarioBD.get().setTelefono(usuario.getTelefono());
-                usuarioBD.get().setFechaNacimiento(usuario.getFechaNacimiento());
-                usuarioRepository.save(usuarioBD.get());
-                return new ResponseEntity(usuarioBD,HttpStatus.OK);
-            }catch (Exception e){
-                return  ResponseEntity.badRequest().build();
-
-            }
-        }
-        return  ResponseEntity.notFound().build();
-
-
+    public ResponseEntity editarUsuario(@PathVariable Long id,@RequestBody @Valid Usuario usuario){
+        return usuarioService.editUser(id,usuario);
     }
     @DeleteMapping (value = "/usuario/{id}")
-    public ResponseEntity eliminarUsuario(@PathVariable Long id){
-        Optional<Usuario> usuarioBD =usuarioRepository.findById(id);
-        if(usuarioBD.isPresent()){
-            usuarioRepository.delete(usuarioBD.get());
-            return  ResponseEntity.noContent().build();
-        }
-        return  ResponseEntity.notFound().build();
+    public ResponseEntity eliminarUsuario(@PathVariable Long id) {
+        return usuarioService.deleteUser(id);
+    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
--- src/main/java/com/procesos/negocio/fabian/models/Usuario.java
+++ src/main/java/com/procesos/negocio/fabian/models/Usuario.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
 @Data
@@ -15,10 +16,13 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column( nullable = false, length = 100)
+    @NotBlank(message = "el nombre no puede ser nulo")
     private String nombre;
     @Column( nullable = false, length = 300)
+    @NotBlank(message = "los apellidos no pueden estar vacios")
     private String apellidos;
     @Column( nullable = false, length = 20, unique = true)
+    @NotBlank(message = "el documento no puede ser nulo")
     private String documento;
     @Column( length = 100)
     private String direccion;
